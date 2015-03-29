8ee465bbb185f2c6100314d9304824241fca69d9
Erin Corson <erin.corson@colorado.edu>
2015-03-29 14:36:01 -0600

    comments and refactoring

--- webapp/app/static/js/app.js
+++ webapp/app/static/js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
 
+    //handles click event for a cell on the game board
     $(".cell").click(function() {
-
+        //get initial cell value so we don't update one that has already
+        //been chosen by a player
         var cellValue = $(this).html().trim();
-        console.log(cellValue);
-        console.log("turn " + turn + " side " + side);
-        console.log(over);
-
-        if (turn == side && cellValue == '' && over != true) {
+        //if the cell hasn't been filled and the game is not over we can make the move
+        if (cellValue == '' && over != true) {
+            //set correct value in clicked cell
             $(this).html(turn);
+            //post clicked position to server to update the db
             $.post( "/update", { id: gameId, turn: turn, loc: $(this).attr('id').slice(5) } ).done(function( data ) {
-                    console.log(data);
+                    //handle win or tie scenarios
                     if (data.winner) {
                         $(".data").html("Game Over! " + data.winner + " wins.");
                         over = true;
@@ -18,22 +19,19 @@ $(document).ready(function() {
                         $(".data").html("Game Over! Tie.");
                         over = true;
                     }
+                    //set pointer to default if game over
                     if (over == true) {
                         $(".cell").css("cursor", "default");
                     }
                 });
-            $("input[name='" + turn + "']").prop("checked", false)
+            //flip turn to next player
             turn = (turn == 'x') ? 'o': 'x';
             side = turn;
+            //set turn indicator
             $("#turn").html(turn);
-            $("input[name='" + turn + "']").prop("checked", true)
-
+            //update instructions
             $(".data").html("Please place an " + side + " on the game board.");
-
         }
-
     });
-
-
 
 });
